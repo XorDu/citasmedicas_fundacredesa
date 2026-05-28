725925a771252f5b77f4058cf3652c8a98813033
XorDu <74994792+XorDu@users.noreply.github.com>
2026-05-28 09:20:51 -0400

    Add backend/models/__init__.py

---
+++ backend/models/__init__.py
@@ -0,0 +1,6 @@
+from backend.models.specialty import Specialty
+from backend.models.permission import Permission
+from backend.models.user import User
+from backend.models.patient import Representative, Patient
+from backend.models.appointment import Appointment
+from backend.models.medical_history import MedicalHistory
